07eb2c88efbe6db381c5aa3ff1ac493369558862
Nyiro, Gergo <gergo.nyiro@oneidentity.com>
2026-04-11 16:34:17 +0200

    ConfigMethod: get

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,41 @@
 use std::{error::Error, future::pending};
 use zbus::{connection, interface};
+use zbus::zvariant::Type;
+use serde::{Serialize, Deserialize};
 
-struct Greeter {
-    count: u64
+#[derive(Serialize, Deserialize, Type)]
+struct Config {
+    some_int: i64,
+    some_string: String,
 }
 
-#[interface(name = "org.zbus.MyGreeter1")]
-impl Greeter {
+struct ConfigMethod {
+    config: Config
+}
+
+#[interface(name = "org.configd.Config")]
+impl ConfigMethod {
     // Can be `async` as well.
-    fn say_hello(&mut self, name: &str) -> String {
-        self.count += 1;
-        format!("Hello {}! I have been called {} times.", name, self.count)
+    fn get(&mut self) -> Config {
+        Config {
+            some_int: self.config.some_int,
+            some_string: self.config.some_string.clone(),
+        }
     }
 }
 
 // Although we use `tokio` here, you can use any async runtime of choice.
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let greeter = Greeter { count: 0 };
+    let config = ConfigMethod {
+        config: Config {
+            some_int: 42,
+            some_string: "hello".to_string(),
+        }
+    };
     let _conn = connection::Builder::session()?
-        .name("org.zbus.MyGreeter")?
-        .serve_at("/org/zbus/MyGreeter", greeter)?
+        .name("org.configd")?
+        .serve_at("/org/configd", config)?
         .build()
         .await?;
 
